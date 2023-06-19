9dd2486166189324c70b0a9149048331a253f9e9
ManasBhole <mbhole@metiscg.com>
2023-06-19 11:33:09 -0400

    fifth commit

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const MainContent = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-black bg-opacity-50 ">
-      <p className="text-start text-white text-4xl italic px-8">We take great pride in providing a safe, environmentally <br/>friendly & truly comfortable charter experience</p>
+    <div className="flex items-center justify-center h-screen bg-black bg-opacity-50">
+      <p className="text-center text-white text-3xl italic px-8 border border-white rounded-lg mx-2 p-4">
+        Hampton Jitney's charter department is dedicated to exceptional service, fulfilling unique transportation needs. Whether it's a corporate event, school trip, wedding, or special occasion, we offer personalized solutions, diverse vehicle options, professional drivers, and a strong focus on safety and reliability. We handle logistics for a seamless experience. Trust us to provide outstanding service and create unforgettable transportation tailored to your needs.
+      </p>
     </div>
   );
 };
